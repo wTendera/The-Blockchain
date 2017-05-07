abeb332ff41323a091ee3658321ee2269d77d988
Wiktor Tendera <wiktortendera@gmail.com>
2017-05-07 18:43:46 +0200

    Connecting to peer holder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const chain = require('./chain')
 
 const Chain = new chain()
+const request = require('superagent')
+const express = require('express')
+const app = express()
+const peers = []
 
-var express = require('express')
-var app = express()
+const port = process.env.PORT || 3000
+
+request.post('localhost:3010/register').send({port: port}).then(res => {
+            console.log("Successfully registered")
+            request.get(`localhost:3010/peers`).then(res => {
+                console.log(`Successfully saved ${res.body.length} peers`)
+                peers.concat(res.body)
+            }, err => console.log("Error while requesting peers:", err))
+        }, err => console.log("Error while registering:", err)
+    )
+
 
 app.get('/', function (req, res) {
-  res.send(`Current Chain: ${JSON.stringify(Chain.blocks)}`)
+    res.send(`Current Chain: ${JSON.stringify(Chain.blocks)}`)
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
 })
